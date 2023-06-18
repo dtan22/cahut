d4fcb98c8f35abc19927cc67f903d24e2a928c90
dtan22 <20120240@student.hcmus.edu.vn>
2023-06-18 07:54:20 +0700

    implement game logic

--- server/index.js
+++ server/index.js
@@ -1,16 +1,21 @@
-require('dotenv').config({ path: '../.env' });
+require('dotenv').config({ path: '../.env' })
 
-const socketio = require("socket.io");
-const express = require('express');
-const bodyparser = require('body-parser');
-const http = require("http");
-const fs = require('fs');
-const cors = require('cors');
+const socketio = require("socket.io")
+const express = require('express')
+const bodyparser = require('body-parser')
+const http = require("http")
+const fs = require('fs')
+const cors = require('cors')
 
-const messages = JSON.parse(fs.readFileSync("../constants/messages.json"));
+const messages = JSON.parse(fs.readFileSync("../constants/messages.json"))
 
-const app = express();
-const server = http.createServer(app);
+const app = express()
+const server = http.createServer(app)
+
+function validatePIN(pinNumber) {
+    // TODO: validate pin number
+    return pinNumber == "1234" || pinNumber == "4321"
+}
 
 const {
     SERVER_PORT,
@@ -18,67 +23,101 @@ const {
     DATABSE_PORT
 } = process.env
 
-app.use(cors());
-app.use(express.json());
-app.use(bodyparser.urlencoded({ extended: false }));
+app.use(cors())
+app.use(express.json())
+app.use(bodyparser.urlencoded({ extended: false }))
+
+const tempQuestions = [
+    {
+        question: "What is the capital of India?",
+        options: ["Delhi", "Mumbai", "Kolkata", "Chennai"],
+        answerIndex: 0
+    },
+    {
+        question: "What is the capital of USA?",
+        options: ["New York", "Washington DC", "Los Angeles", "Chicago"],
+        answerIndex: 1
+    }
+]
+
+function getQuestion(pinNumber, questionIndex) {
+    // TODO: get question from database
+    return tempQuestions[questionIndex]
+}
 
 const socketIo = socketio(server, {
     cors: {
         origin: "*",
     }
-});
+})
 
 socketIo.on("connection", (socket) => {
-    console.log("New client connected, id: " + socket.id);
+    console.log("New client connected, id: " + socket.id)
 
-    socket.emit(messages.SERVER_SEND_ID, socket.id);
+    socket.emit(messages.SERVER_SEND_ID, socket.id)
 
-    socket.on(messages.CLIENT_HOST_JOIN, (data) => {
-        const { pinNumber } = data;
-        socket.join(pinNumber);
-        socket.join(pinNumber + "-host");
-        if (pinNumber == "1234") {
-            socket.emit(SERVER_JOIN_RESPONSE, { success: true, message: "successful" })
-        } else {
-            socket.emit(SERVER_JOIN_RESPONSE, { success: false, message: "unsuccessful" })
-        }
-    });
+    socket.on(messages.CLIENT_HOST_WAIT, (data) => {
+        const { pinNumber } = data
+        socket.join(pinNumber)
+        socket.join(pinNumber + "-host")
+    })
+
+    socket.on(messages.CLIENT_HOST_NEXT_QUESTION, (data) => {
+        const { pinNumber, questionIndex } = data
+        const question = getQuestion(pinNumber, questionIndex)
+        socket.to(pinNumber).emit(messages.SERVER_QUESTION_START, { question, questionIndex })
+        setTimeout(async () => {
+            const data = await getTurnStatistics(pinNumber, questionIndex)
+            socket.to(pinNumber).emit(messages.SERVER_QUESTION_END, data)
+        }, 10000)
+    })
+
+    socket.on(messages.CLIENT_HOST_END, (data) => {
+        const { pinNumber } = data
+        socket.to(pinNumber).emit(messages.SERVER_GAME_END)
+    })
 
     socket.on(messages.CLIENT_PLAYER_JOIN, (data) => {
-        const { pinNumber } = data;
-        if (pinNumber == "1234") {
-            socket.join(pinNumber);
+        const { pinNumber } = data
+        if (validatePIN(pinNumber)) {
+            socket.join(pinNumber)
             socket.emit(messages.SERVER_JOIN_RESPONSE, { success: true, message: "successful", pinNumber: pinNumber })
         } else {
             socket.emit(messages.SERVER_JOIN_RESPONSE, { success: false, message: "unsuccessful" })
         }
-    });
+    })
+
+    socket.on(messages.CLIENT_PLAYER_ANSWER, (data) => {
+        const { pinNumber, questionIndex, answerIndex } = data
+        const question = getQuestion(pinNumber, questionIndex)
+        socket.to(pinNumber).emit(messages.SERVER_PLAYER_ANSWER, { correct: answerIndex == question.answerIndex })
+    })
 
     socket.on(messages.CLIENT_SEND_CHAT_MESSAGE, function (data) {
-        const { message, id, pinNumber } = data;
+        const { message, id, pinNumber } = data
         console.log(data)
-        socketIo.to(pinNumber).emit(messages.SERVER_SEND_CHAT_MESSAGE, { message, id });
+        socketIo.to(pinNumber).emit(messages.SERVER_SEND_CHAT_MESSAGE, { message, id })
     })
 
     socket.on("disconnect", () => {
-        console.log("Client disconnected");
-    });
-});
+        console.log("Client disconnected")
+    })
+})
 
 
 server.listen(SERVER_PORT, () => {
-    console.log(`Server running at http://localhost:${SERVER_PORT}/`);
-});
+    console.log(`Server running at http://localhost:${SERVER_PORT}/`)
+})
 
 app.post('/player-join', (req, res) => {
     try {
-        const { pinNumber } = req.body;
-        if (pinNumber == "1234") {
-            res.status(200).send({ success: true, message: "successful.", pinNumber: pinNumber });
+        const { pinNumber } = req.body
+        if (validatePIN(pinNumber)) {
+            res.status(200).send({ success: true, message: "successful.", pinNumber: pinNumber })
         } else {
-            res.status(200).send({ success: false, message: "no such PIN number exists." });
+            res.status(200).send({ success: false, message: "no such PIN number exists." })
         }
     } catch (error) {
-        res.status(500).send({ success: false, message: error.message });
+        res.status(500).send({ success: false, message: error.message })
     }
-});
+})
